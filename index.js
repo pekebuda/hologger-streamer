b90dd95710fef01ae2bc819d89f42cc19a08fc1b
Fernando Diaz Laclaustra <f.diazlaclaustra@umlaud.es>
2017-10-12 12:10:33 +0200

    Implementa clase principal.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-module.exports = {};
+/**
+ * Module dependencies.
+ */
+var Streamer = require('./lib/streamer');
+
+
+/**
+ * Expose `Streamer` directly from package.
+ */
+exports = module.exports = Streamer;
